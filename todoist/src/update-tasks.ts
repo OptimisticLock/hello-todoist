23fc7039e46a8807e1966b329bd78475913310ca
Irina Rapoport <izGithub@optimisticLock.com>
2025-04-18 18:55:54 -0700

    Adds label

--- todoist/src/update-tasks.ts
+++ todoist/src/update-tasks.ts
@@ -5,19 +5,34 @@ const fs = require('fs').promises;
 
 console.log("Hello!");
 const apiToken = process.env.TOKEN;
+let totalCount = 0;
 
 const fetchTasks = async () => {
   try {
+    console.log("Fetching Tasks from todoist.com");
+    let url = 'https://todoist.com/api/v1/tasks';
+    while (true) {
+      const response = await axios.get(url, { headers: {Authorization: `Bearer ${apiToken}`} });
+      const count = response.data.results.length;
+      totalCount += count;
+      console.log(`Got ${count} tasks from todoist.com. Total: ${totalCount}`);
+      await processTasks(response.data.results);
+      console.log("Processed response from todoist");
+      let cursor = response.data.next_cursor;
+      if (!cursor) {
+        console.log("No more tasks to process");
+        break;
+      }
+      url = 'https://todoist.com/api/v1/tasks?cursor=' + cursor;
+      console.log("Next cursor: ", cursor);
+      console.log("Next URL: ", url);
+      console.log("Fetching next page of tasks");
+    }
 
  //   const updateTaskResult = await updateTask('4629630727', {"content": "Buy Coffee " + new Date(), labels: ["api123"]});
  //   console.log("Task updated: " + updateTaskResult);
     //const url = 'https://api.todoist.com/rest/v2/tasks';
-    const url = 'https://todoist.com/api/v1/tasks?limit=30';
-    console.log("Fetching Tasks from todoist.com");
-    const response = await axios.get(url, { headers: {Authorization: `Bearer ${apiToken}`} });
-    console.log("Got tasks from todoist.com");
-    await processTasks(response.data);
-    console.log("Processed response from todoist");
+
     console.log("Done");
 
   } catch (error) {
@@ -38,27 +53,33 @@ async function processTasks(tasks: any) {
   let count = 0;
 
   for (const task of tasks) {
+//    console.log("####### Task: ", task);
     console.log(`Task: ${count++}: ${task.content}`);
     const id = task.id;
     const dueString = task?.due?.string;
     const dueLabel = task?.due?.string?.replaceAll(" ", "-"); // ?.replace("every", "every-");
    // const was = {...task};
-    const labels = [... task.labels, "api_v1"];
+    const labels = [... task.labels, "api_v1_take2"];
+    const newLabels = labels;
+ //   const newLabels = labels.filter(label => !label.startsWith("every-"));
 
     if (dueLabel)
-      labels.push(dueLabel);
+      newLabels.push(dueLabel);
     else
-      console.log("wtf");
+      console.log("no due label for task: ", task.content);
 
-    const result = await updateTask(task.id, {labels});
-    console.log("was: ", task, "now: ", result);
+
+  //  if (labels.length !== newLabels.length) {
+      const result = await updateTask(task.id, {labels: newLabels});
+      console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Task modified:", task.content, "now: ", result);
+ //   }
   }
 
-  const jsonString = JSON.stringify(tasks, null, 4);
- // console.log('Tasks:', response.data);
-  // console.log("String: ", jsonString);
-  await fs.writeFile('ignoreMe/todoist-tasks.json', jsonString);
-  console.log('Tasks have been saved to ignoreMe/todoist-tasks.json');
+//   const jsonString = JSON.stringify(tasks, null, 4);
+//  // console.log('Tasks:', response.data);
+//   // console.log("String: ", jsonString);
+//   await fs.writeFile('ignoreMe/todoist-tasks.json', jsonString);
+//   console.log('Tasks have been saved to ignoreMe/todoist-tasks.json');
 
 }
 
